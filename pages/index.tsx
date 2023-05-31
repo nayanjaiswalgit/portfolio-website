fff434f3a630ffc6e158f0017a8bf946fb77ab6c
Nayan <jaiswal2062@gmail.com>
2023-05-31 11:07:49 +0530

    project closed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import logo from '@/public/logo.png'
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className=" h-[100vh] w-full flex  flex-col justify-center items-center gap-4 bg-gradient-to-t
@@ -8,7 +9,7 @@ export default function Home() {
         <div className="  text-center font-semibold opacity-95 ">
            <Image src={logo} width={200}  alt='logo ' className=' rounded-2xl border-2'></Image> </div>
         <div className='text-center' ><h1 className="font-bold text-3xl ">Welcome To My Website</h1>
-        <button className='py-3 px-14 rounded-lg  bg-green-600 text-white m-5'>See My Projects</button>
+        <button className='py-3 px-14 rounded-lg  bg-green-600 text-white m-5'><Link href="/projects/agriarm/dashboard">See My Projects</Link></button>
         </div>
         <div>
 
